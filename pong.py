37923be6c87de779923bdbbf9cd8be63d35f7025
Georgie <zgeorgiez@gmail.com>
2019-11-30 19:03:07 -0500

    Paddle B and Ball

--- pong.py
+++ pong.py
@@ -20,8 +20,24 @@ paddle_a.penup()
 paddle_a.goto(-350, 0)
 
 # Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+
 
 # Main game loop
 
